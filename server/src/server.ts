8f878b6e01d23a33559e4815b88b85ee42210e16
Antonio <antoniomatallc@gmail.com>
2025-02-17 01:51:33 -0800

    Save progress before rebase

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,31 +1,8 @@
-// import express from 'express';
-// import routes from './routes/index.js';
-// import cors from "cors";
-// import dotenv from "dotenv";
-
-
-// const app = express();
-// const PORT = process.env.PORT || 5003;
-
-// app.use(express.json());
-// app.use('/', routes);  // ✅ Correctly register all routes
-
-// app.listen(PORT, () => {
-//     console.log(`Server running on http://localhost:${PORT}`);
-// });
-
-// app.use(
-//     cors({
-//       origin: "http://localhost:5173", // Allow frontend origin
-//       methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
-//       credentials: true, // Allow cookies
-//     })
-//   );
-  
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth-routes.js";
+import ticketRoutes from './routes/api/ticket-routes';
 
 dotenv.config();
 
